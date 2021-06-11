47caec2c03ce276867e955e8e7775d3cdfb8fd44
isaac666 <isaacwu@live.com>
2021-06-11 21:43:06 +0800

    helloworld的创建页html页面的编写

---
+++ entry/src/main/java/xyz/youtubeyunying/myapplication/MyApplication.java
@@ -0,0 +1,10 @@
+package xyz.youtubeyunying.myapplication;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
